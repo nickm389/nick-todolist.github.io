a7dd469319743024f6d87c56f936582f0443bba2
Nick <78834590+nickm389@users.noreply.github.com>
2025-03-06 21:37:31 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+
+document.addEventListener('DOMContentLoaded',function(){
+
+    var task_lists = []; 
+
+    document.querySelector('#new-task').onsubmit = function(event){
+                
+        const li = document.createElement('li'); 
+        let task_text = document.querySelector('#task').value; 
+        let task_priority = document.querySelector('#priority').value; 
+        let task_status = "pending"; 
+
+        let new_task_html = `<span class="priority"> <b>${task_text}</b> <u class="priority">${task_priority}</u> <i>${task_status}</i></u></span>
+                             <input type="radio" class="complete" name="task-complete" /> Task Completed   
+                             <button class="remove" type="button">Remove</button> 
+                                                
+        `;
+        li.innerHTML=new_task_html 
+
+        document.querySelector('#tasks-list').append(li); 
+        document.querySelector('#task').value = ''; 
+
+        console.log(task_lists); 
+       
+        return false; 
+
+    }
+
+    document.addEventListener('click', function(event){
+        element = event.target;
+        if (element.className == 'remove'){
+            element.parentElement.remove(); 
+        }
+        if (element.className == 'complete'){
+            const task = element.parentElement.querySelector('span');
+            const status = task.querySelector('i');
+            const text = task.querySelector('b');
+
+            status.innerText = 'completed';
+            status.style.color = 'green';
+            text.style.textDecoration = 'line-through';
+        }
+    })
+})
+
